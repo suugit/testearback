a921f0de54636dd255c34ff7d6f381e8b55fa00f
supportlaver <ak4686@naver.com>
2024-09-05 13:41:35 +0900

    init setting

---
+++ src/main/java/com/example/ear/EarApplication.java
@@ -0,0 +1,13 @@
+package com.example.ear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ear/EarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
